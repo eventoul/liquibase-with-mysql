720800a711e9c6d9537138342d7e872c832e78d2
eventoul <bagb88@gmail.com>
2025-04-09 11:43:01 +0300

    Initial commit

---
+++ src/main/java/com/mysql/liquibase/mysql/LiquibaseWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.liquibase.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseWithMySqlApplication.class, args);
+	}
+
+}
