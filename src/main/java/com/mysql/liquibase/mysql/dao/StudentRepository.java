e3566528c3d5b065740589cfb825acb20468d4ef
eventoul <bagb88@gmail.com>
2025-04-09 12:22:12 +0300

    Add Student repository.

---
+++ src/main/java/com/mysql/liquibase/mysql/dao/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.mysql.liquibase.mysql.dao;
+
+import com.mysql.liquibase.mysql.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  StudentRepository extends JpaRepository<Student, Long> {
+
+}
